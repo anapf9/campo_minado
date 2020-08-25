1cd39930101c12dad54334394d215ab473daae4f
Ana <ana.eng.dev@gmail.com>
2020-08-25 09:33:05 -0300

    adding the amount bombs

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ document.addEventListener('DOMContentLoaded', () => {
   let squares = []
   // create Board
   function createBoard() {
-    // get shuffled game array with random bombs
     // O método fill() preenche todos os valores do array a partir do índice inicial a um índice final com um valor estático
+    // Usar classList é uma alternativa conveniente para acessar a lista de classes de um elemento como uma seqüência delimitada por espaço através de element.className
     const bombsArray = Array(bombAmount).fill('bomb')
     const emptyArray = Array(width * width - bombAmount).fill('valid')
     const gameArray = emptyArray.concat(bombsArray)
@@ -18,6 +18,41 @@ document.addEventListener('DOMContentLoaded', () => {
       square.classList.add(shuffleArray[i])
       grid.appendChild(square)
       squares.push(square)
+    }
+
+    // add numbers
+    for (let i = 0; i < squares.length; i++) {
+      let total = 0
+      const isLeftEdge = (i % width === 0) // se sobrar 0 na divisão
+      const isRightEdge = (i % width === width - 1) // se a divisão por 10 sobrar 9 
+      if (squares[i].classList.contains('valid')) {
+        if (i > 0 && !isLeftEdge && squares[i - 1].classList.contains('bomb')) {
+          total++
+        }
+        if (i > 9 && !isRightEdge && squares[i + 1].classList.contains('bomb')) {
+          total++
+        }
+        if (i > 10 && squares[i - width].classList.contains('bomb')) {
+          total++
+        }
+        if (i > 11 && !isLeftEdge && squares[i - 1 - width].classList.contains('bomb')) {
+          total++
+        }
+        if (i < 98 && !isRightEdge && squares[i + 1].classList.contains('bomb')) {
+          total++
+        }
+        if (i < 90 && !isLeftEdge && squares[i - 1 + width].classList.contains('bomb')) {
+          total++
+        }
+        if (i < 88 && !isRightEdge && squares[i + 1 + width].classList.contains('bomb')) {
+          total++
+        }
+        if (i < 89 && squares[i + width].classList.contains('bomb')) {
+          total++
+        }
+        squares[i].setAttribute('data', total)
+        console.log(squares)
+      }
     }
   }
   createBoard()
