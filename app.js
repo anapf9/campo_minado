1a4e5139984488263e1ca1b709f48b0b9080b2be
Ana <ana.eng.dev@gmail.com>
2020-08-25 08:50:21 -0300

    shuffle array

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
   const grid = document.querySelector('.grid')
   let width = 10
+  let bombAmount = 20
   let squares = []
   // create Board
   function createBoard() {
+    // get shuffled game array with random bombs
+    // O método fill() preenche todos os valores do array a partir do índice inicial a um índice final com um valor estático
+    const bombsArray = Array(bombAmount).fill('bomb')
+    const emptyArray = Array(width * width - bombAmount).fill('valid')
+    console.log(bombsArray, emptyArray)
+
+    const gameArray = emptyArray.concat(bombsArray)
+    const shuffleArray = gameArray.sort(() => Math.random() - 0.5)
+    console.log(gameArray, shuffleArray)
+
     for (let i = 0; i < width * width; i++) {
       const square = document.createElement('div')
       square.setAttribute('id', i)
